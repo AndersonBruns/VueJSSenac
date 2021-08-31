730ecb6ecafbb22864e4f665a7ea6a81e589c92f
Anderson Bruns <30638794+AndersonBruns@users.noreply.github.com>
2021-08-30 21:33:09 -0300

    add-and-remove-project

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,16 @@
     <div v-if="mostrarInfoComplementar">
       <h2>Informação adicional</h2>
 
+     <form @submit.prevent="addProject()">
+        <label for="nome">Nome:</label><br/>
+        <input type="text" id="nome" autofocus required v-model="projeto.nome"/><br/>
+        <label for="tamanho">Tamanho:</label><br/>
+        <input type="number" id="tamanho" required min="0" max="100" v-model="projeto.tamanho"/><br/>
+        <input type="submit" id="btn" value="Adicionar projeto"/>
+      </form> 
+
       <ul>
-          <li v-for="p in projetos" :key="p.nome"> {{p.nome}}</li>
+          <li v-for="p in projetos" :key="p.nome"> {{p.nome}} <button @click="remove(p)"> Remover </button></li>
       </ul>
     </div>
 
@@ -30,15 +38,34 @@ export default {
         {nome: 'HTML', tamanho: 100},
         {nome: 'CSS', tamanho: 80},
         {nome: 'JS', tamanho: 200}
-        ]
+        ],
+
+        projeto:{
+          nome: null,
+          tamanho: 0
+        }
     }
   },
 
   methods:{
     showInfo(){
       this.mostrarInfoComplementar = !this.mostrarInfoComplementar;
+    },
+
+    addProject(){
+      this.projetos.push({
+        nome: this.projeto.nome,
+        tamanho: this.projeto.tamanho
+      })
+    },
+
+    remove(projetoRemove){
+      if(confirm("Deseja remover?")){
+        this.projetos.splice(this.projetos.indexOf(projetoRemove), 1)
+      }
     }
-  }
+
+  },
 
 }
 </script>
